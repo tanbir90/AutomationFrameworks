c2d0fbd429ea2d8abcb996c5afdeb1f4c813b626
tanbir90 <hellotanbir90@gmail.com>
2019-09-19 03:29:12 +0530

    first commit

---
+++ src/main/java/com/crm/stepDefinitions/Steps.java
@@ -0,0 +1,39 @@
+/*
+ * package com.crm.stepDefinitions;
+ * 
+ * import cucumber.api.PendingException; import cucumber.api.java.en.Given;
+ * import cucumber.api.java.en.Then;
+ * 
+ * public class Steps {
+ * 
+ * @Given("^user opens browser$") public void user_opens_browser() throws
+ * Throwable { // Write code here that turns the phrase above into concrete
+ * actions throw new PendingException(); }
+ * 
+ * @Then("^user is on login page$") public void user_is_on_login_page() throws
+ * Throwable { // Write code here that turns the phrase above into concrete
+ * actions throw new PendingException(); }
+ * 
+ * @Then("^user enters username and password$") public void
+ * user_enters_username_and_password() throws Throwable { // Write code here
+ * that turns the phrase above into concrete actions throw new
+ * PendingException(); }
+ * 
+ * @Then("^user clicks on login button$") public void
+ * user_clicks_on_login_button() throws Throwable { // Write code here that
+ * turns the phrase above into concrete actions throw new PendingException(); }
+ * 
+ * @Then("^home page is displayed$") public void home_page_is_displayed() throws
+ * Throwable { // Write code here that turns the phrase above into concrete
+ * actions throw new PendingException(); }
+ * 
+ * @Then("^validate logged in user$") public void validate_logged_in_user()
+ * throws Throwable { // Write code here that turns the phrase above into
+ * concrete actions throw new PendingException(); }
+ * 
+ * @Then("^close the browser$") public void close_the_browser() throws Throwable
+ * { // Write code here that turns the phrase above into concrete actions throw
+ * new PendingException(); }
+ * 
+ * }
+ */
---
+++ src/test/java/com/crm/features/crmLogin.feature
@@ -0,0 +1,28 @@
+#Author: your.email@your.domain.com
+#Keywords Summary :
+#Feature: List of scenarios.
+#Scenario: Business rule through list of steps with arguments.
+#Given: Some precondition step
+#When: Some key actions
+#Then: To observe outcomes or validation
+#And,But: To enumerate more Given,When,Then steps
+#Scenario Outline: List of steps for data-driven as an Examples and <placeholder>
+#Examples: Container for s table
+#Background: List of steps run before each of the scenarios
+#""" (Doc Strings)
+#| (Data Tables)
+#@ (Tags/Labels):To group Scenarios
+#<> (placeholder)
+#""
+## (Comments)
+#Sample Feature Definition Template
+
+Feature: CRM application login test
+
+Scenario: Title of your scenario
+  Given user is already on Login Page
+#  Then title of login page is Free CRM
+#  Then user enters "<username>" and "<password>"
+#  Then user clicks on login button
+  Then Close the browser
+
